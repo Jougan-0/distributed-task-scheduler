eaa0766cb59ed95031b7be494d7d9486e8dbc0a3
Jougan-0 <prasantmishra2018@gmail.com>
2025-03-03 06:57:44 +0530

    chore: dummy endpoint

---
+++ ui/src/app/api/dummy/route.ts
@@ -0,0 +1,4 @@
+export async function GET() {
+    return new Response("This is a dummy API endpoint.", { status: 200 });
+  }
+  
--- ui/src/app/logs/page.tsx
+++ ui/src/app/logs/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 export const dynamic = "force-dynamic";
-
+export const revalidate = 0;
 import { useWebSocketContext } from "../WebSocketProvider";
 export default function LogsPage() {
   const { logs } = useWebSocketContext();
