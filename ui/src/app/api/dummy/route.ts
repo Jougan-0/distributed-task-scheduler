e06137639514b720c5b8636a95f8d62a76a0a9cd
Jougan-0 <prasantmishra2018@gmail.com>
2025-03-03 07:03:06 +0530

    chore: remove revalidate=0

--- ui/src/app/api/dummy/route.ts
+++ ui/src/app/api/dummy/route.ts
@@ -1,4 +1,10 @@
 export async function GET() {
-    return new Response("This is a dummy API endpoint.", { status: 200 });
+    return new Response(JSON.stringify({ time: new Date().toISOString() }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store, max-age=0, must-revalidate",
+      },
+    });
   }
   
